2e726c8a1053be63993c06fb7ecc5f35fd0b84f8
billymckey <maxbutsaev@gmail.com>
2025-10-20 16:57:34 +0300

    update20.10

--- MyBookTracker/MyBookTracker.cpp
+++ MyBookTracker/MyBookTracker.cpp
@@ -10,6 +10,7 @@ struct BOOKS
 {
 	char date[11];
 	char name[40];
+	char author[40];
 	float grade;
 };
 
@@ -22,15 +23,18 @@ void loadBooks(const char* filename)
 	if (!fin)
 		return;
 	bookkolvo = 0;
-	string date_str, name_str, grade_str;
+	string date_str, name_str, author_str, grade_str;
 	while (getline(fin, date_str, '|') &&
 		getline(fin, name_str, '|') &&
+		getline(fin, author_str, '|') &&
 		getline(fin, grade_str))
 	{
 		if (date_str.length() < 11)
 			date_str.copy(books[bookkolvo].date, date_str.length());
 		books[bookkolvo].date[date_str.length()] = '\0';
-
+		if (author_str.length() < 40)
+			author_str.copy(books[bookkolvo].author, author_str.length());
+		books[bookkolvo].author[author_str.length()] = '\0';
 		if (name_str.length() < 40)
 			name_str.copy(books[bookkolvo].name, name_str.length());
 		books[bookkolvo].name[name_str.length()] = '\0';
@@ -50,7 +54,7 @@ void saveBOOKSToFIle(const char* filename)
 	}
 	for (int i = 0; i < bookkolvo; i++)
 	{
-		fout << books[i].date << "|" << books[i].name << "|" << books[i].grade << endl;
+		fout << books[i].date << "|" << books[i].name << "|" << books[i].author << "|" << books[i].grade << endl;
 	}
 	fout.close();
 }
@@ -65,6 +69,9 @@ void addBook()
 
 	cout << "дата (гггг.дд.мм) ";
 	cin >> e.date;
+
+	cout << "Автор (И.О.Фамилия) ";
+	cin >> e.author;
 
 	cout << "введите название ";
 	cin.ignore();
@@ -86,14 +93,16 @@ void showallbooks()
 	}
 
 	cout << "\n ---ВСЕ КНИГИ---\n";
-	cout << left << setw(12) << "дата"
-		<< setw(15) << "название "
+	cout << left << setw(15) << "дата"
+		<< setw(25) << "название "
+		<< setw(30) << "автор "
 		<< "оценка\n";
-	cout << "===================\n";
+	cout << "========================================================================================\n";
 	for (int i = 0; i < bookkolvo; i++)
 	{
-		cout << left << setw(12) << books[i].date
-			<< setw(15) << books[i].name
+		cout << left << setw(15) << books[i].date
+			<< setw(25) << books[i].name
+			<< setw(30) << books[i].author
 			<< books[i].grade << " баллов\n";
 	}
 }
@@ -115,14 +124,14 @@ void showgrades()
 		}
 	}
 	float avg = total / bookkolvo;
-	cout << "\n---  статистика ---\n";
+	cout << "\n----  статистика ----\n";
 	cout << "Всего книг " << bookkolvo << " шт.\n";
 	cout << "Средняя оценка " << avg << " баллов\n";
 	cout << "Максимальная оценка " << max << " баллов\n";
 }
 void showMenu()
 {
-	cout << "\n--- Трекер книг ---\n";
+	cout << "\n---- Трекер книг ----\n";
 	cout << "1) Добавить книгу\n";
 	cout << "2) Показать все книги\n";
 	cout << "3) Показать всю статистику\n";
